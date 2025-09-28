1dc05d6b019c996d03b2cb2476bf036fba7a1d37
Александр <shura.botlife@gmail.com>
2025-09-28 15:20:04 +0300

    chore: created a convenient structure for the project

--- TeleAuto/src/teleauto/credentials.py
+++ TeleAuto/src/teleauto/credentials.py
@@ -7,7 +7,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
-CREDENTIALS_FILE = "credentials.json"
+CREDENTIALS_FILE = "../../credentials.json"
 
 def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
--- TeleAuto/src/teleauto/main.py
+++ TeleAuto/src/teleauto/main.py
@@ -1,8 +1,8 @@
 ﻿import time
 import pyotp
-from login import login_telemart
+from src.teleauto.login.login import login_telemart
 from credentials import input_credentials, load_credentials, verify_pin, decrypt_credentials
-import vpn
+from src.teleauto.vpn import vpn
 
 TOTP_OFFSET = 40  # скорректируйте при необходимости
 
@@ -33,16 +33,26 @@ def main():
             username, password, secret_2fa = decrypt_credentials(creds, None)
 
     vpn.start_pritunl()
-    proc = vpn.connect_vpn()
     vpn.click_pritunl_connect()
 
     totp_code = get_current_totp(secret_2fa)
     if not vpn.input_2fa_code_and_reconnect(totp_code):
         print("Не удалось ввести 2FA код и нажать вторую кнопку Connect")
 
-    vpn.wait_for_connection()
+    time.sleep(5)
 
-    login_telemart(username, password)
+    # Получаем IP с любого активного адаптера Pritunl
+    print("\nПолучение IP адреса активного адаптера Pritunl...")
+    ip_vpn = vpn.get_first_tap_adapter()
+
+    if ip_vpn:
+        print(f"Используем IP для проверки подключения: {ip_vpn}")
+        if vpn.vpn_connect_with_retries(ip_vpn, totp_code):
+            print("VPN подключен успешно!")
+            login_telemart(username, password)
+        else:
+            print("Не удалось подключиться к VPN")
+
     time.sleep(10)
 
 
